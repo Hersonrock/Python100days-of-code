4186530a2896f0d08b5d7f7fb9bc4a5b46daddf2
Hersonrock <hjpg20@gmail.com>
2022-10-09 19:45:54 -0500

    Band Name generator v1.0

--- Band Name Generator.py
+++ Band Name Generator.py
@@ -1,1 +1,7 @@
-test
+#Band Name generator Python.
+# I will be changing the questions, "What's the name of the city you grew up in" and "Name of a pet" since it sounds too much like social engineering secret questions stuff...
+
+print("Welcome to Band Name Generator")
+fristAnswer = input("What is your favorite Chemical Element? \n")
+secondAnswer = input("What feeling does it evoke? \n")
+print("Your band shall be called: " + fristAnswer + " " + secondAnswer)
