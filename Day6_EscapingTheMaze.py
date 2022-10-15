51096e9fc780bfa652de945b7301f650b76c33f4
Hersonrock <hjpg20@gmail.com>
2022-10-15 10:20:29 -0500

    Day6 escapingthemaze project completed

--- Day6_EscapingTheMaze.py
+++ Day6_EscapingTheMaze.py
@@ -1,5 +1,17 @@
+#This code is for https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json 
 
-def my_function():
-    print("Hello World")
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
 
-my_function()
+    
+# while not at_goal():
+      
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#          turn_left()
