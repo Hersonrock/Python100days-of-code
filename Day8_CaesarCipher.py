f3e9f30f819bf4f95a53f3ea17e27a330b1bbf00
Hersonrock <hjpg20@gmail.com>
2022-10-16 09:49:31 -0500

    Day8 caesarcipher, almost complete, but ignoring last character when encoding or decoding

--- Day8_CaesarCipher.py
+++ Day8_CaesarCipher.py
@@ -4,16 +4,18 @@ mode=""
 outputMessage=""
 continueCipherstr="no"
 continueCipher=True
-#97=a ,122=z
+
 def encodeMessage():
     inputMessage=input("Type your messsage: ").lower()
-    shiftNumber=input("Type the shift number: ")
+    shiftNumber=int(input("Type the shift number: "))
+    shifter(inputMessage,shiftNumber)
     print("Here's the encoded result: "+outputMessage)
 
 def decodeMessage():
     inputMessage=input("Type your messsage: ").lower()
-    shiftNumber=input("Type the shift number: ")
-    print("Here's the decoded result: "+outputMessage)
+    shiftNumber=int(input("Type the shift number: "))
+    shifter(inputMessage,-shiftNumber)
+    print("Here's the decoded result: "+ outputMessage)
 
 def continueGameQuestion():
     continueCipherstr=input("Type \'yes\' if you want to go again. Otherwise type \'no\': ")
@@ -22,10 +24,26 @@ def continueGameQuestion():
     elif continueCipherstr.lower()=="yes":
         continueCipher=True
 
-def shifter(shiftNumber):
+def shifter(message,shiftNumber):
+    #Will work using ASCII
+    # 97=a ,122=z
+    
+    global outputMessage
+    for i in range(1,len(message)+1):
+        charOrder= ord(message[i-2:i-len(message)-1])+shiftNumber
+        print(message[i-1:i-len(message)]+ " world")
+        while charOrder < 97 :
+            charOrder += 26  #123-97
+        while charOrder > 122:
+            charOrder -= 26  #123-97e
+
+    
+        outputMessage += chr(charOrder)
+
 
 
 while continueCipher:
+    
     mode=input("Type \'encode\' to encrypt, type \'decode\' to decrypt: ").lower()
     if mode=="encode":
         encodeMessage()
@@ -33,5 +51,6 @@ while continueCipher:
         decodeMessage()
     else:
         print("Wrong input")
+    outputMessage=""
     continueGameQuestion()
     
