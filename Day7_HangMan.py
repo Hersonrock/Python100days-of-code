26006ccb84dcd16629759b5bf5c5ea0c38fba589
Hersonrock <hjpg20@gmail.com>
2022-10-15 22:01:41 -0500

    Day7 hangman, add basic gameplay, lose condition is broken

--- Day7_HangMan.py
+++ Day7_HangMan.py
@@ -4,11 +4,60 @@
 #Using "requests" , installed with command "pip install requests"
 #   see: https://www.geeksforgeeks.org/get-post-requests-using-python/
 
+from tkinter.tix import Tree
 import requests
 randomWordURL = "https://random-word-api.herokuapp.com/word"
 randomWordRAW = requests.get(url=randomWordURL)
 randomWordStringRaw= randomWordRAW.text
 randomWordString=randomWordStringRaw[2:-2]
 print (randomWordString)
+guessWord= []
+lives=10
+guessLetter=""
+index = -1
+hit= False
 
+for character in randomWordString:
+    guessWord.append("_")
+
+def update ():
+    print(guessWord)
+    print("(lives:", end='') 
+    for i in range(lives):
+        print ("*", end='')
+    print(")")
+    print("\n")
+
+
+def solved():
+    i=len(randomWordString)
+    for character in guessWord:
+        if character=="_":
+            return False
+        else:
+            i -= 1
+    if i==0:
+        return True
+
+print("Welcome to HangMan, will you guess in time?")
+update()
+
+while not solved() or lives==0:
+    guessLetter=input("Guess a letter: ")
+    for randomWordLetter in randomWordString:
+        index += 1
+        if guessLetter == randomWordLetter:
+            guessWord[index]=guessLetter
+            hit=True
+    if hit==False:
+        lives -= 1
+    index= -1
+    update()
+    
+
+
+if solved():
+    print("You Win!!!")
+else:
+    print("You Lose,try again")
 
