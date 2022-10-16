d8b21f62f99fd4ed52b2b36fa97dc38548347d8f
Hersonrock <hjpg20@gmail.com>
2022-10-16 08:45:15 -0500

    Day8 caesarcipher, adds lower case check on input

--- Day8_CaesarCipher.py
+++ Day8_CaesarCipher.py
@@ -4,14 +4,14 @@ mode=""
 outputMessage=""
 continueCipherstr="no"
 continueCipher=True
-
+#97=a ,122=z
 def encodeMessage():
-    inputMessage=input("Type your messsage: ")
+    inputMessage=input("Type your messsage: ").lower()
     shiftNumber=input("Type the shift number: ")
     print("Here's the encoded result: "+outputMessage)
 
 def decodeMessage():
-    inputMessage=input("Type your messsage: ")
+    inputMessage=input("Type your messsage: ").lower()
     shiftNumber=input("Type the shift number: ")
     print("Here's the decoded result: "+outputMessage)
 
@@ -21,6 +21,9 @@ def continueGameQuestion():
         continueCipher=False
     elif continueCipherstr.lower()=="yes":
         continueCipher=True
+
+def shifter(shiftNumber):
+
 
 while continueCipher:
     mode=input("Type \'encode\' to encrypt, type \'decode\' to decrypt: ").lower()
