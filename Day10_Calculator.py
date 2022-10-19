8f3a0249614544b2fdf381cb36d3bad98456538c
Hersonrock <hjpg20@gmail.com>
2022-10-18 22:03:30 -0500

    Day10 calculator, Project started, working calculator with basic functions

---
+++ Day10_Calculator.py
@@ -0,0 +1,54 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(n1, n2):
+    return n1+n2
+
+def subtract(n1, n2):
+    return n1-n2
+
+def multiply(n1, n2):
+    return n1*n2
+
+def divide(n1, n2):
+    if n2 !=0:
+        return n1/n2
+    else:
+        print("Cannot divide by 0")
+        return 
+    
+
+operations = {
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide,
+}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol=input("Pick an operation from the line above: ")
+
+#This seems hard to understand, but the dictionary entry can be used to refer to the function by just its name.
+#The name is then substituted, based on the first value pair (the symbol)
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print (f"{num1} {operation_symbol} {num2} = {answer}")
