8b66ec9b62153b54a75a80c30a4e1811543dd083
Hersonrock <hjpg20@gmail.com>
2022-10-24 15:59:37 -0500

    Day11, adds comments and function Docstrings

--- Day11_Blackjack.py
+++ Day11_Blackjack.py
@@ -21,10 +21,12 @@ continue_playing=True
 cards=[11,2,3,4,5,6,7,8,9,10,10,10]
 
 def refresh():
+    """refreshes console"""
     os.system('cls')
     print(logo)
 
 def bet_start():
+    """asks for the player initial bet ammount and returns bank remaining ammount"""
     total=bank_total
     bet_amount=0
     while bet_amount==0:
@@ -36,6 +38,7 @@ def bet_start():
     return total
     
 def continue_playing_function():
+    """checks if the player wants to continue playing"""
     continue_playing_raw=""
     while continue_playing_raw!="y" and continue_playing_raw!="n":
         continue_playing_raw=input("Do you want to continue playing? (y/n): ").lower()
@@ -48,11 +51,12 @@ def continue_playing_function():
     continue_playing_raw=""
 
 def random_deal():
+    """deals a random card from the cards list"""
     first_deal= random.randint(0,11)
     return cards[first_deal]
 
 def ace_check(hand):
-
+    """Returns ace index if there is an ace on the hand AND if the hand sum goes over 21, returns -1 when nothing is found"""
     index=-1
     if sum(hand)>21:
         for card in hand:
@@ -80,7 +84,7 @@ def over_21(hand):
         return True
 
 def hit():
-
+    """checks if the player wants an extra card \"hit\" or not \"stand\" """
     askForCard=None
    
     while askForCard==None:
@@ -95,7 +99,7 @@ def hit():
     return askForCard
 
 def print_hand(hand,isPlayer,isFirstRound):
-
+    """Function will print based on the list Hand, if the print if for a player and if it is the first round or not"""
     for i in range(0,len(hand)):
         if i==0:
             if isPlayer:
@@ -117,10 +121,11 @@ while continue_playing:
     print("Welcome to BlackJack!")
     print("Shuffling...")
     print("Bank: $"+str(bank_total))
+
+    #Initializing variables
     bet_total= bank_total-bet_start()
     bank_total -= bet_total
     bet_multiplier=2
-
     dealerHand=[]
     playerHand=[]
     playerWon=-2
@@ -129,17 +134,14 @@ while continue_playing:
     for i in range(0,2):
         dealerHand.append(random_deal())
         playerHand.append(random_deal())
-        
-
     if ace_check(dealerHand)!=-1:
         dealerHand[ace_check(dealerHand)]=1
     if ace_check(playerHand)!=-1:
         playerHand[ace_check(playerHand)]=1    
-    
     print_hand(dealerHand,False,True)
     print_hand(playerHand,True,True)
 
-    
+    #Natural Win initial check, a winner can be defined here by the first deal.
     if check_21(dealerHand) or check_21(playerHand):
         if check_21(dealerHand) and check_21(playerHand):
             print("Is a Tie")
@@ -152,8 +154,9 @@ while continue_playing:
             playerWon=1
             bank_total+=bet_total*bet_multiplier
             print("Bank: $"+str(bank_total))
-
+    #Game CoreLoop ; player -2=None, -1=Tie, 0=Loss, 1=Win
     while playerWon==-2:
+        #If the player wants to stay with current hand , computer will loop untill game is outcome is defined.
         if not hit():
             while sum(dealerHand)<=sum(playerHand) and under_21(dealerHand):
                 dealerHand.append(random_deal())
@@ -168,6 +171,7 @@ while continue_playing:
             elif check_21(dealerHand) or sum(dealerHand)>sum(playerHand):
                 print("You Lose")
                 playerWon=0
+        #If player wants another hand, a random card is appended and win conditions are checked, until player wins, losses or asks for another card.
         else:
             playerHand.append(random_deal())
             if ace_check(playerHand)!=-1:
@@ -182,6 +186,7 @@ while continue_playing:
                 bank_total+=bet_total*bet_multiplier
                 print("Bank: $"+str(bank_total))
 
+# main loop is reset if the player wants, player keeps current money, or is reset to initial ammount in case it reaches $0
     if bank_total>0:
         continue_playing=continue_playing_function()
     else:
