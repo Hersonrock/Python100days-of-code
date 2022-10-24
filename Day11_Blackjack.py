03e772bde791aabaccb417d3b828943f25505140
Hersonrock <hjpg20@gmail.com>
2022-10-24 07:52:46 -0500

    Fixes aceCheck() bug

--- Day11_Blackjack.py
+++ Day11_Blackjack.py
@@ -12,14 +12,15 @@ logo = """
 from ctypes import sizeof
 import os
 import random
+from tkinter import N
 
 #HINT: Clearing the Screen
 #   os.system('cls')
 
 bank_total=1000
 continue_playing=True
-#cards=[11,2,3,4,5,6,7,8,9,10,10,10]
-cards=[11,11,11,11,11,10,10,10,10,10,10,10]
+cards=[11,2,3,4,5,6,7,8,9,10,10,10]
+
 def refresh():
     os.system('cls')
     print(logo)
@@ -59,6 +60,8 @@ def ace_check(hand):
             index += 1
             if card ==11:
                 return index
+            else:
+                return -1
     else:
         return -1
 
@@ -69,10 +72,53 @@ def  sumHand(hand):
         sum +=card
     return sum
 
-def check21(hand):
+# def check21(hand):
 
-    if sumHand(hand)==21:
-        return True
+#     if sumHand(hand)==21:
+#         return True
+
+# def under21(hand):
+
+#     if sumHand(hand)<21:
+#         return True
+
+# def over21(hand):
+
+#     if sumHand(hand)>21:
+#         return True
+
+def hit():
+
+    askForCard=None
+   
+    while askForCard==None:
+        answer= input("Hit or Stand?: ").lower()
+        if answer=="hit":
+            askForCard=True
+        elif answer=="stand":
+            askForCard=False
+        else:
+            print("Incorrect option, try again")
+
+    return askForCard
+
+# def  printHand(hand,isPlayer,isFristRound):
+
+#     for i in range(0,len(hand)):
+#         if i==0:
+#             if isPlayer:
+#                 print(f"Player Hand: {hand[i]},",end="")
+#             else:
+#                 print(f"Dealer Hand: {hand[i]},",end="")
+#         elif i==len(hand)-1:
+#             if isPlayer:
+#                 print(f"{hand[i]} = {sumHand(hand)}")
+#             elif isFristRound:
+#                 print("X")
+#             else:
+#                 print(f"{hand[i]} = {sumHand(hand)}")
+#         else:
+#             print(f"{hand[i]},",end="")
 
 while continue_playing:
     refresh()
@@ -83,7 +129,7 @@ while continue_playing:
 
     dealerHand=[]
     playerHand=[]
-    gameRound=0
+
 
     #Computer and player first deal
     for i in range(0,2):
@@ -96,17 +142,28 @@ while continue_playing:
     if ace_check(playerHand)!=-1:
         playerHand[ace_check(playerHand)]=1    
     
+    # printHand(dealerHand,False,True)
+    # printHand(playerHand,True,True)
     print(f"Dealer Hand: {dealerHand[0]},{dealerHand[1]}")
     print(f"Player Hand: {playerHand[0]},{playerHand[1]}")
     
     if check21(dealerHand) or check21(playerHand):
         if check21(dealerHand) and check21(playerHand):
-            print("Is a tie")
+            print("Is a Tie")
         elif check21(dealerHand):
-            print("You lose")
+            print("You Lose")
         elif check21(playerHand):
             print("You Win")
 
-
-
+    # if not hit():
+    #     while sumHand(dealerHand)<sumHand(playerHand) and under21(dealerHand):
+    #         dealerHand.append(random_deal())
+    #         if ace_check(dealerHand)!=-1:
+    #             dealerHand[ace_check(dealerHand)]=1
+    #     printHand(dealerHand,False,False)
+    #     if over21(dealerHand):
+    #         print("You Win")
+    #     elif check21(dealerHand) or sumHand(dealerHand)>sumHand(playerHand):
+    #         print("You Lose")
+    
     continue_playing=continue_playing_function()
