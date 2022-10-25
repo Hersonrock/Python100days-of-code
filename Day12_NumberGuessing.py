8327cb3c1c312c3d05307a9ef31f39ce81ab510d
Hersonrock <hjpg20@gmail.com>
2022-10-25 07:44:43 -0500

    Day12, numberguess project start

---
+++ Day12_NumberGuessing.py
@@ -0,0 +1,40 @@
+
+from random import random
+from tkinter.messagebox import NO
+
+
+logo="""
+ _______               ___.                    ________                             
+ \      \  __ __  _____\_ |__   ___________   /  _____/ __ __   ____   ______ ______
+ /   |   \|  |  \/     \| __ \_/ __ \_  __ \ /   \  ___|  |  \_/ __ \ /  ___//  ___/
+/    |    \  |  /  Y Y  \ \_\ \  ___/|  | \/ \    \_\  \  |  /\  ___/ \___ \ \___ \ 
+\____|__  /____/|__|_|  /___  /\___  >__|     \______  /____/  \___  >____  >____  >
+        \/            \/    \/     \/                \/            \/     \/     \/
+"""
+
+import random
+
+
+difficulty=None
+attempts=0
+secret=random.randint(1,100)
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+while (difficulty==None):
+    difficulty_raw=input("Choose a difficulty. Type \'easy\' or \'hard\': ").lower()
+    if (difficulty_raw=="easy"):
+        difficulty=1
+    elif (difficulty_raw=="hard"):
+        difficulty=2
+    else:
+        print("Wrong option")
+
+if difficulty==1:
+    attempts=10
+elif difficulty==2:
+    attempts=5
+
+print(f"The secret number is : {secret}")
+print(f"The difficulty is {difficulty}")
