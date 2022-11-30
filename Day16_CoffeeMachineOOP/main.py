c59d7979c03e28b70825675d36871b234eecd58c
Hersonrock <hjpg20@gmail.com>
2022-11-29 19:45:33 -0500

    Day16 project completed

--- Day16_CoffeeMachineOOP/main.py
+++ Day16_CoffeeMachineOOP/main.py
@@ -1,3 +1,33 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+is_on= True
+coffe_machine=CoffeeMaker()
+money_machine=MoneyMachine()
+menu=Menu()
+
+
+# choice=input("What would you like? (espresso/latte/cappuccino): ").lower()
+# if choice=="report":
+#     coffe_machine.report()
+#     money_machine.report()
+# elif choice=="espresso" or choice== "latte" or choice =="cappuccino":
+
+while is_on:
+
+    options= menu.get_items()
+    choice=input(f"What would you like? ({options}): ").lower()
+    if choice=="report":
+        coffe_machine.report()
+        money_machine.report()
+    elif choice=="off":
+        is_on =False
+    else:
+         drink=menu.find_drink(choice)
+         if coffe_machine.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffe_machine.make_coffee(drink)
+
+print("Good Bye...")
