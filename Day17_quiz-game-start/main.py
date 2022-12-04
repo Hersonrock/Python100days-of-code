639568793c6495883e4b2c26e971da2b202a9101
Hersonrock <hjpg20@gmail.com>
2022-12-04 02:57:48 -0500

    Day17, adds automatic questions from API

--- Day17_quiz-game-start/main.py
+++ Day17_quiz-game-start/main.py
@@ -1,13 +1,31 @@
-from data import question_data
+#Using quiz API from
+#   https://opentdb.com/api_config.php
+# 
+#Using "requests" , installed with command "pip install requests"
+#   see: https://www.geeksforgeeks.org/get-post-requests-using-python/
+
+import requests
+import json # used for manipulating JSON data
+
+from data import question_data  # This is not really needed 
 from question_model import Question
 from quiz_brain import QuizBrain
+
+ammount=10
+quiz_list_url = f"https://opentdb.com/api.php?amount={ammount}&category=15&difficulty=hard&type=boolean"
+quiz_list = requests.get(url=quiz_list_url).json()
 
 question_bank=[]
 i = 0
 
-for question in question_data:
-    question_bank.append(Question(question["text"],question["answer"]))
+for question in quiz_list["results"]:
+    question["question"]=question["question"].replace("&quot;",'"')
+    question["question"]=question["question"].replace("&#039;","'")
+    question_bank.append(Question(question["question"],question["correct_answer"]))
 
 quiz=QuizBrain(question_bank)
 
-quiz.next_question()
+while quiz.still_has_question():
+    quiz.next_question()
+
+print (f"You completed the quiz \nYour final score was {QuizBrain.score}/{len(QuizBrain.question_list)}")
--- Day17_quiz-game-start/quiz_brain.py
+++ Day17_quiz-game-start/quiz_brain.py
@@ -2,11 +2,25 @@ class QuizBrain:
 
     question_list = []
     question_number=0
+    score=0
     def __init__(self,questions):
         QuizBrain.question_list=questions
     def next_question(self):
 
         current_question = QuizBrain.question_list[QuizBrain.question_number]
         QuizBrain.question_number += 1
-        input(f"Q.{QuizBrain.question_number}: {current_question.text} (True/False)")
-        
+        user_answer= input(f"Q.{QuizBrain.question_number}: {current_question.text} (True/False): ").lower()
+        self.check_answer(user_answer)
+
+    def still_has_question(self):
+
+        return QuizBrain.question_number<len(QuizBrain.question_list)
+
+    def check_answer(self,answer):
+        if QuizBrain.question_list[QuizBrain.question_number-1].answer.lower() == answer:
+            print ("You got it right!")
+            QuizBrain.score +=1
+        else:
+            print ("That's wrong!")
+        print(f"The correct answer is {QuizBrain.question_list[QuizBrain.question_number-1].answer} ")
+        print(f"Score: {QuizBrain.score}/{QuizBrain.question_number} \n")
