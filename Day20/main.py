6bdc13fc3458721b56f762cf0757f25076e407a5
Hersonrock <hjpg20@gmail.com>
2022-12-28 09:36:45 -0500

    Day20, organizes code and adds comments

--- Day20/main.py
+++ Day20/main.py
@@ -6,24 +6,28 @@ screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Snek")
-screen.tracer(0)
+screen.tracer(0)  #Is necesary for animation control.
 game_on=True
 
-
-screen.listen()
-      
 Snek=Snake()
+
+
+#----------Screen Input handling--------
+screen.listen()
+screen.onkey(Snek.left,"Left")
+screen.onkey(Snek.right,"Right")
+screen.onkey(Snek.up,"Up")
+screen.onkey(Snek.down,"Down")     
+#I don't yet understand how this can be called while game is running on game loop. 
+#The logic of Screen.listen seems to be running in parallel?
+#In hindsight it makes more sense that Screen is always running and everything else is running as a child of it, but at the same time??
+#/--------------------------------------
+
 
 while game_on:
 
-    screen.onkey(Snek.left,"Left")
-    screen.onkey(Snek.right,"Right")
-    screen.onkey(Snek.up,"Up")
-    screen.onkey(Snek.down,"Down")
-
-    Snek.move() 
-    screen.update()
+    screen.update()  #Update goes along with tracer for animation control. Especifies the time the screen is updated.
     time.sleep(0.1) 
-
+    Snek.move() 
 
 screen.exitonclick()
--- Day20/snake.py
+++ Day20/snake.py
@@ -1,54 +1,63 @@
 from turtle import Turtle
 STARTING_POSITIONS=[(0,0),(-20,0),(-40,0)]
+MOVE_DISTANCE=20
+UP= 90
+DOWN=270
+LEFT=180
+RIGHT=0
 
 class Snake:
 
-    
     def __init__(self):
         self.segments=[]
         self.create_snake()
-
+        
+        self.head=self.segments[0]  #sets snake head for convenience.
 
     def create_snake(self):
 
         for position in STARTING_POSITIONS:
-            
+
+            #Initializes all segments and keeps turtle hidden.
             segment=Turtle(shape="square")
             segment.color("white")
             segment.hideturtle()
             segment.penup()
             segment.goto(position)
             segment.showturtle()
-            self.segments.append(segment) #A way to track segments and to keep them organized.
-        self.head=self.segments[0]
 
+            self.segments.append(segment) #A way to track segments and to keep them organized.
+        
+
+#------------Segment Follow-----------------------
+# Moves head forward, and pulls all remaining segments to the position of the one ahead of it.
     def move(self):
         last_loc=self.head.position()
         self.move_forwards()
+
         for i in range(1,len(self.segments)):
             new_loc=last_loc
             last_loc=self.segments[i].position()
             self.segments[i].goto(new_loc)
-        
+#/-------------------------------------------------      
+
+#---------------Movement control-------------------
     def move_forwards(self):
-        self.head.forward(20)
+        self.head.forward(MOVE_DISTANCE)
 
     def right(self):
         if self.head.heading()!=180:
-            new_heading=0
-            self.head.seth(new_heading)
+            self.head.seth(RIGHT)
+
     def left(self):
         if self.head.heading()!=0:
-            new_heading=180
-            self.head.seth(new_heading)
+            self.head.seth(LEFT)
             
     def up(self):
         if self.head.heading()!=270: 
-            new_heading=90
-            self.head.seth(new_heading)
+            self.head.seth(UP)
             
     def down(self):
         if self.head.heading()!=90:
-            new_heading=270
-            self.head.seth(new_heading)
-        
+            self.head.seth(DOWN)
+#/-------------------------------------------------     
