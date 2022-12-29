35f4cee1f008caadd7461d3e0afd4587c5ba1fa7
Hersonrock <hjpg20@gmail.com>
2022-12-28 23:25:54 -0500

    Day21, improves scoreboard test alignment and size

--- Day20/scoreboard.py
+++ Day20/scoreboard.py
@@ -5,13 +5,13 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score=0
         self.hideturtle()
-        self.goto(-40,250)
+        self.goto(0,250)
         self.color("white")
-        self.write(f"Score: {self.score}",font=('Arial',12))
+        self.write(f"Score: {self.score}",align="center",font=('Arial',24))
         self.penup()
         
         
 
     def increase_score(self):
         self.score +=1
-        self.write(f"Score: {self.score}",font=('Arial',12))
+        self.write(f"Score: {self.score}",align="center",font=('Arial',24))
