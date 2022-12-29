18b7b39981b85b9facad5572c1439688c8cf5acc
Hersonrock <hjpg20@gmail.com>
2022-12-29 12:14:06 -0500

    Day22, bounce added

--- Day22/ball.py
+++ Day22/ball.py
@@ -1,9 +1,9 @@
 from turtle import Turtle
-
-# from main import SCREEN_HEIGHT,SCREEN_WIDTH
+import random
 
 SCREEN_HEIGHT=600
 SCREEN_WIDTH=600
+HEADING_CHANGE=20
 
 UP= 90
 DOWN=270
@@ -22,15 +22,56 @@ class Ball(Turtle):
         
     def move(self,speed):
         self.forward(speed)
+            
+
+    def bounce(self,touch,heading):
+        if heading==UP or heading==DOWN or heading==LEFT or heading==RIGHT:
+            self.setheading(heading+random.randrange(-HEADING_CHANGE,HEADING_CHANGE,HEADING_CHANGE)+180)
+        elif(heading>0 and heading<90):
+            if touch=="top":
+                self.setheading(heading-90)
+            if touch=="right":
+                self.setheading(heading+90)
+        elif(heading>90 and heading<180):
+            if touch=="top":
+                self.setheading(heading+90)
+            if touch=="left":
+                self.setheading(heading-90)
+        elif(heading>180 and heading<270):
+            if touch=="bot":
+                self.setheading(heading-90)
+            if touch=="left":
+                self.setheading(heading+90)
+        elif(heading>270 and heading<360):
+            if touch=="bot":
+                self.setheading(heading+90)
+            if touch=="right":
+                self.setheading(heading-90)
+             
+        print(self.heading())
 
     def collision(self):
 
         if self.position()[0]>(SCREEN_WIDTH/2)-10:
-            self.setheading(LEFT)
+            #self.setheading(LEFT)
             self.color("red")
+            self.touch="right"
+            self.bounce(self.touch,self.heading())
         if self.position()[0]<-(SCREEN_WIDTH/2)+10:
-            self.setheading(RIGHT)
+             #self.setheading(RIGHT)
             self.color("blue")
+            self.touch="left"
+            self.bounce(self.touch,self.heading())
+        if self.position()[1]>(SCREEN_HEIGHT/2)-10:
+             #self.setheading(DOWN)
+            self.color("green")
+            self.touch="top"
+            self.bounce(self.touch,self.heading())
+        if self.position()[1]<-(SCREEN_HEIGHT/2)+10:
+             #self.setheading(UP)
+            self.color("yellow")
+            self.touch="bot"
+            self.bounce(self.touch,self.heading())
 
 
     
