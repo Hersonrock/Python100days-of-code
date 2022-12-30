b7e2ddf6f446fc411c4a93aac3ccfb696f52f169
Hersonrock <hjpg20@gmail.com>
2022-12-30 09:14:48 -0500

    Day23, adds street delimiters

--- Day23/main.py
+++ Day23/main.py
@@ -1,11 +1,23 @@
 
 from scoreboard import Scoreboard,SCREEN_HEIGHT,SCREEN_WIDTH
 from turtleCross import TurtleCross
-from turtle import Screen
+from turtle import Screen,Turtle
 import time
 
-game_on=True
 
+def street_create():
+    painter=Turtle()
+    painter.hideturtle()
+    painter.penup()
+    painter.goto(-SCREEN_WIDTH/2,SCREEN_HEIGHT/2-100)
+    painter.pendown()
+    painter.forward(SCREEN_WIDTH)
+    painter.penup()
+    painter.goto(-SCREEN_WIDTH/2,-SCREEN_HEIGHT/2+100)
+    painter.pendown()
+    painter.forward(SCREEN_WIDTH)
+
+game_on=True
 
 screen=Screen()
 screen.setup(width=SCREEN_WIDTH,height=SCREEN_HEIGHT)
@@ -18,7 +30,7 @@ scoreboard=Scoreboard()
 
 screen.listen()
 screen.onkey(turtle.move,"w")
-
+street_create()
 
 while game_on:
     
