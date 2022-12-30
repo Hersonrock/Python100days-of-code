7f0ca2450a4a4bffbb2b8c7a56d07e338513ba78
Hersonrock <hjpg20@gmail.com>
2022-12-30 10:02:17 -0500

    Day23, adds car class

--- Day23/cars.py
+++
@@ -1,4 +0,0 @@
-from turtle import Turtle
-
-
-class Car
--- Day23/main.py
+++ Day23/main.py
@@ -2,6 +2,8 @@
 from scoreboard import Scoreboard,SCREEN_HEIGHT,SCREEN_WIDTH
 from turtleCross import TurtleCross
 from turtle import Screen,Turtle
+from car import Car
+import random
 import time
 
 
@@ -17,16 +19,22 @@ def street_create():
     painter.pendown()
     painter.forward(SCREEN_WIDTH)
 
+def rand_y():
+    y_position=random.randint(-SCREEN_HEIGHT/2+110,SCREEN_HEIGHT/2-110)
+    return y_position
+
 game_on=True
+counter=1
 
 screen=Screen()
 screen.setup(width=SCREEN_WIDTH,height=SCREEN_HEIGHT)
 screen.bgcolor("white")
 screen.title("Turtle Cross")
 screen.tracer(0)
+
 turtle=TurtleCross()
 scoreboard=Scoreboard()
-
+car=Car(rand_y())
 
 screen.listen()
 screen.onkey(turtle.move,"w")
@@ -35,4 +43,10 @@ street_create()
 while game_on:
     
     screen.update()
+    car.move()
     time.sleep(0.1)
+    if counter<100:
+        counter +=1
+    else:
+        counter=1
+   
