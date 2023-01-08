c3dd3b07cefc68252b0f38f39538bfae738e365f
Hersonrock <hjpg20@gmail.com>
2023-01-08 15:19:57 -0500

    Day24 adds savefile for snake

--- Day24_files_diectories_paths/Snake+Project+Code+from+Day+21/scoreboard.py
+++ Day24_files_diectories_paths/Snake+Project+Code+from+Day+21/scoreboard.py
@@ -8,7 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score=0
+        self.high_score= int(self.read_file())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,8 +22,22 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score>self.high_score:
             self.high_score=self.score
+            self.write_file(self.high_score)
         self.score=0
         self.update_scoreboard()
+
+    def read_file(self):
+        score_file = open("E:\Python 100 days\Day24_files_diectories_paths\Snake+Project+Code+from+Day+21\data.txt")
+        raw_file_content= score_file.read()
+        score_file.close()
+        return raw_file_content
+
+    def write_file(self,score):
+        score_file = open("E:\Python 100 days\Day24_files_diectories_paths\Snake+Project+Code+from+Day+21\data.txt",mode="w")
+        score_file.write(str(score))
+        score_file.close()
+
+        
 
 
     # def game_over(self):
