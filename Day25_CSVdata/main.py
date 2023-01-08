a99a45dcf46827b8c647195a616b240ebba20c4b
Hersonrock <hjpg20@gmail.com>
2023-01-08 17:28:37 -0500

    Day25,creating a dataframe

--- Day25_CSVdata/main.py
+++ Day25_CSVdata/main.py
@@ -15,14 +15,35 @@
 # print(temperatures)
 
 import pandas
+#https://pandas.pydata.org/docs/reference/
 
 data = pandas.read_csv("Day25_CSVdata\weather_data.csv")
 temp_list=data["temp"].to_list()
 
 temp_sum=0
 
-print(data["temp"].mean())
-print(data["temp"].max())
+def to_f(c):
+    return (c*9/5)+32
+
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# monday=data[data.day=="Monday"]
+# monday_temp=int(monday.temp)
+
+# print(to_f(monday_temp))
 
 # temp_sum=sum(temp_list)
 # print(f"average temp: {temp_sum/len(temp_list)}")
+
+
+#create a dataframe
+
+data_dic = {
+    "students": ["Amy","Angela","Karen"],
+    "scores":[76,56,65]
+}
+
+data=pandas.DataFrame(data_dic)
+
+data.to_csv(".\Day25_CSVdata\\new_data.csv")
