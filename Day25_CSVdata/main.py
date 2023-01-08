a07a7856643aff77ab7947abb88a2cecb4229c76
Hersonrock <hjpg20@gmail.com>
2023-01-08 16:59:58 -0500

    Day25, priting rows

---
+++ Day25_CSVdata/main.py
@@ -0,0 +1,11 @@
+# with open("Day25_CSVdata\weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+
+import csv
+
+with open ("Day25_CSVdata\weather_data.csv") as data_file:
+    data= csv.reader(data_file)
+    for row in data:
+        print(row) 
