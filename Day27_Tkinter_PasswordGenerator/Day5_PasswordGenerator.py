f27ffa07a6fd627ea442df31aa1455e17dfa748b
Hersonrock <hjpg20@gmail.com>
2023-01-14 10:56:04 -0500

    Day27 PasswordGen(Tkinter), removes console input/output

--- Day27_Tkinter_PasswordGenerator/Day5_PasswordGenerator.py
+++ Day27_Tkinter_PasswordGenerator/Day5_PasswordGenerator.py
@@ -4,7 +4,7 @@ import random
 
 window = Tk()
 window.title("Password Generator")
-window.minsize(width=800,height=600)
+window.minsize(width=500,height=300)
 
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -13,11 +13,9 @@ SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 def build_pass(nr_letters,nr_symbols,nr_numbers):
     totalCharacters= range(1,nr_letters+nr_symbols+nr_numbers+1)
     password = []
-    print(f"totalCharacters= {len(totalCharacters)}")
     for character in totalCharacters:
         appended=False
         while appended==False:
-            print(f"{character}")
             randomChoice= random.randint(1,3)
             if randomChoice==1 and nr_letters>0:
                 password.append(LETTERS[random.randint(0,len(LETTERS)-1)])
@@ -34,8 +32,15 @@ def build_pass(nr_letters,nr_symbols,nr_numbers):
     return password
 
 def print_password():
+    #Gathering entry content
+    nr_letters=int(letter_entry.get())
+    nr_symbols =int(symbols_entry.get())
+    nr_numbers = int(numbers_entry.get())
+    #Building password out of content choosen and printing as a label.
     password=build_pass(nr_letters,nr_symbols,nr_numbers)
-    print("Your password is: "+"".join(password))
+    password_ouput="Your password is: "+"".join(password)
+    output=Label(text=password_ouput,font=("Arial",10))
+    output.grid(column=0,row=7)
 
 
 # print("Welcome to the PyPassword Generator!")
@@ -43,21 +48,34 @@ welcome=Label(text="Welcome to the PyPassword Generator!", font=("Arial",15,))
 welcome.grid(column=0,row=0)
 welcome.config(padx=20,pady=20)
 
-
+# Changing console input to Label+Entry
 # nr_letters= int(input("How many letters would you like in your password?\n")) 
 letter_label=Label(text="How many letters would you like in your password?",font=("Arial",10))
 letter_label.grid(column=0,row=1)
-letter_label.config(padx=20,pady=20)
-letter_entry=Entry(width=20)
+letter_label.config(padx=5,pady=5)
+letter_entry=Entry(width=5)
 letter_entry.insert(END,string=len(LETTERS))
 letter_entry.grid(column=0,row=2)
-nr_letters=int(letter_entry.get())
 
+# Changing console input to Label+Entry
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+symbols_label=Label(text="How many symbols would you like?", font=("Arial",10))
+symbols_label.grid(column=0,row=3)
+symbols_label.config(padx=5,pady=5)
+symbols_entry=Entry(width=5)
+symbols_entry.insert(END,string=len(SYMBOLS))
+symbols_entry.grid(column=0,row=4)
 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+# Changing console input to Label+Entry
+#nr_numbers = int(input(f"How many numbers would you like?\n"))
+numbers_label=Label(text="How many numbers would you like?",font=("Arial",10))
+numbers_label.grid(column=0,row=5)
+numbers_label.config(padx=5,pady=5)
+numbers_entry=Entry(width=5)
+numbers_entry.insert(END,string=len(NUMBERS))
+numbers_entry.grid(column=0,row=6)
 
-
+#Definining Button and function
 print_pass_btn=Button(text="Print Password",command=print_password)
 print_pass_btn.grid(column=1,row=0)
 
