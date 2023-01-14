4c150f67ae3dda7ac1f9582ddd6aec4cf438e2ec
Hersonrock <hjpg20@gmail.com>
2023-01-14 10:37:52 -0500

    Day27 PasswordGen(Tkinter), working prototype, with mixed colsole and GUI

--- Day27_Tkinter_PasswordGenerator/Day5_PasswordGenerator.py
+++ Day27_Tkinter_PasswordGenerator/Day5_PasswordGenerator.py
@@ -6,9 +6,37 @@ window = Tk()
 window.title("Password Generator")
 window.minsize(width=800,height=600)
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+def build_pass(nr_letters,nr_symbols,nr_numbers):
+    totalCharacters= range(1,nr_letters+nr_symbols+nr_numbers+1)
+    password = []
+    print(f"totalCharacters= {len(totalCharacters)}")
+    for character in totalCharacters:
+        appended=False
+        while appended==False:
+            print(f"{character}")
+            randomChoice= random.randint(1,3)
+            if randomChoice==1 and nr_letters>0:
+                password.append(LETTERS[random.randint(0,len(LETTERS)-1)])
+                nr_letters -= 1
+                appended=True
+            if randomChoice==2 and nr_symbols>0:
+                password.append(SYMBOLS[random.randint(0,len(SYMBOLS)-1)])
+                nr_symbols -= 1
+                appended=True
+            if randomChoice==3 and nr_numbers>0:
+                password.append(NUMBERS[random.randint(0,len(NUMBERS)-1)])
+                nr_numbers -= 1
+                appended=True
+    return password
+
+def print_password():
+    password=build_pass(nr_letters,nr_symbols,nr_numbers)
+    print("Your password is: "+"".join(password))
+
 
 # print("Welcome to the PyPassword Generator!")
 welcome=Label(text="Welcome to the PyPassword Generator!", font=("Arial",15,))
@@ -16,40 +44,22 @@ welcome.grid(column=0,row=0)
 welcome.config(padx=20,pady=20)
 
 
+# nr_letters= int(input("How many letters would you like in your password?\n")) 
 letter_label=Label(text="How many letters would you like in your password?",font=("Arial",10))
 letter_label.grid(column=0,row=1)
 letter_label.config(padx=20,pady=20)
 letter_entry=Entry(width=20)
-letter_entry.insert(END,string=f"From 1 to {len(letters)}")
+letter_entry.insert(END,string=len(LETTERS))
 letter_entry.grid(column=0,row=2)
+nr_letters=int(letter_entry.get())
 
 
-nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = []
-appended = False
-totalCharacters= range(1,nr_letters+nr_symbols+nr_numbers+1)
 
-for character in totalCharacters:
-    appended=False
-    while appended==False:
-        randomChoice= random.randint(1,3)
-        if randomChoice==1 and nr_letters>0:
-            password.append(letters[random.randint(0,len(letters)-1)])
-            nr_letters -= 1
-            appended=True
-        if randomChoice==2 and nr_symbols>0:
-            password.append(symbols[random.randint(0,len(symbols)-1)])
-            nr_symbols -= 1
-            appended=True
-        if randomChoice==3 and nr_numbers>0:
-            password.append(numbers[random.randint(0,len(numbers)-1)])
-            nr_numbers -= 1
-            appended=True
-
-print("Your password is: "+"".join(password))
+print_pass_btn=Button(text="Print Password",command=print_password)
+print_pass_btn.grid(column=1,row=0)
 
 
 window.mainloop()
