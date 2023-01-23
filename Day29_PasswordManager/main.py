691a650c95121e835d74f9d48804177a9e89429d
Hersonrock <hjpg20@gmail.com>
2023-01-23 00:38:31 -0500

    Day29, adds basic window, dimentions, title and image

--- Day29_PasswordManager/main.py
+++ Day29_PasswordManager/main.py
@@ -1,5 +1,20 @@
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window=Tk()
+window.title("Password Manager")
+window.minsize(width=220,height=220)
+window.config(padx=20,pady=20,bg="white")
+
+canvas=Canvas(width=200,height=200,bg="white",highlightthickness=0)
+
+logo_img=PhotoImage(file="Day29_PasswordManager\\logo.png")
+canvas.create_image(100,95,image=logo_img)
+canvas.grid(column=0,row=0)
+
+window.mainloop()
