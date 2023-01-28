79da3c1bab708986002377ef438016c89568c960
Hersonrock <hjpg20@gmail.com>
2023-01-28 08:35:35 -0500

    adds comments and improves readability

--- Day2_LifeInWeeks(mini_project).py
+++ Day2_LifeInWeeks(mini_project).py
@@ -1,15 +1,28 @@
-# 🚨 Don't change the code below 👇
-age = input("What is your current age?")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
-maxAge=90
-realAge=int(age)
-yearLeft=maxAge-realAge
-monthsLeft=yearLeft*12
-weeksLeft=yearLeft*52
-daysLeft=yearLeft*365
+MAX_AGE=90
 
-message=(f"You have {daysLeft} days, {weeksLeft} weeks, and {monthsLeft} months left.")
+# Type and bound check.
+while True:
+    try:
+        age = int(input("What is your current age? (between 0 and 90): "))
+        # Exit loop only if no exception and criteria is meet.
+        if 0<= age <=90:   
+            break   
+        else:
+            print("Age must be between 0 and 90. Please try again!")
+    except ValueError:
+        print("Age must be an integer. Please try again!")
+
+
+years_left=MAX_AGE-age
+months_left=years_left*12
+weeks_left=years_left*52
+days_left=years_left*365
+
+message=(
+    f"You have {days_left} days or {weeks_left} weeks or " 
+    f"{months_left} months left until you reach {MAX_AGE} years old"
+    )
+
 
 print(message)
