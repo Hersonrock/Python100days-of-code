04e4d896bb2b05e753aa9beb152e5a7159d00cfb
Hersonrock <hjpg20@gmail.com>
2023-01-28 08:35:00 -0500

    Improves readability

--- Day1_Band Name Generator.py
+++ Day1_Band Name Generator.py
@@ -1,6 +1,8 @@
 print("Welcome to Band Name Generator")
+
 fristAnswer = input("What is your favorite Chemical Element?: ")
 secondAnswer = input("What feeling does it evoke?: ")
 
 band_name= " ".join([fristAnswer, secondAnswer])
+
 print( f"Your band shall be called: {band_name}")
